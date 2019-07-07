39d67d8d912b0475b3ea00a10b241144ef5925eb
Viktor <meitlin@gmail.com>
2019-07-07 23:00:19 +0300

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Filter } from './models/filter';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tickets';
+  
+  checkedFilters: Array<number> = [];
+
+  filters : Array<Filter>= [
+    {
+      title: "Беза пересадок",
+      value: "0"
+    },
+    {
+      title: "1 пересадка",
+      value: "1"
+    },
+    {
+      title: "2 пересадки",
+      value: "2"
+    },
+    {
+      title: "3 пересадки",
+      value: "3"
+    }
+  ];
+
+
+  filterChange (values:any) {
+    
+    if(values.currentTarget.checked) {
+      this.checkedFilters.push(values.currentTarget.value);
+    }
+    else {
+      for(var i=0 ; i < this.checkedFilters.length; i++) {
+          if(this.checkedFilters[i] == values.currentTarget.value) {
+            this.checkedFilters.splice(i,1);
+        }
+      }
+    }
+    
+  }
 }
